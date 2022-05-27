9bb5cfcab6c2d1b9e5e7f38c8877404ed8726307
Enias Oliveira <eniasoliveira27@gmail.com>
2022-05-27 01:48:27 -0300

    chore: nix shell setup

---
+++ shell.nix
@@ -0,0 +1,41 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+with pkgs;
+
+let inherit (lib) optional optionals;
+
+in mkShell {
+  buildInputs = [ elixir nodejs-16_x postgresql_12 niv ]
+    ++ optional stdenv.isLinux inotify-tools
+    ++ optional stdenv.isDarwin terminal-notifier ++ optionals stdenv.isDarwin
+    (with darwin.apple_sdk.frameworks; [ CoreFoundation CoreServices ]);
+
+  LOCALE_ARCHIVE_2_27 = "${glibcLocales}/lib/locale/locale-archive";
+
+  shellHook = ''
+    export LANG="en_US.UTF-8"
+
+    export PGDATA=$PWD/db
+    export PGHOST=$PWD/postgres
+    export PGDATABASE=github_repo_data_dev
+    export PGPORT=5439
+
+    export DATABASE_URL="postgresql:///$PGDATABASE?host=$PGHOST&port=$PGPORT"
+    export LOG_PATH=$PWD/postgres/LOG
+
+    if [ ! -d $PGHOST ]; then
+      mkdir -p $PGHOST
+    fi
+
+    if [ ! -d $PGDATA ]; then
+      initdb --auth=trust --no-locale --encoding=UTF8
+    fi
+
+    postgres_start() {
+      if ! pg_ctl status
+      then
+        pg_ctl start -l $LOG_PATH -o "-c unix_socket_directories='$PGHOST'"
+      fi
+    }
+  '';
+}
